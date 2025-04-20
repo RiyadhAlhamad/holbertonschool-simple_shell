45936478e24bbfc458b6be1732963c5c0a14d419
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-21 02:29:56 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,113 +1,69 @@
 #include "shell.h"
 
-/**
- * main - The entry point of the shell program
- * Return: Always 0
+/*
+ * File: main.c
+ * Description: Entry point for a simple shell.
+ * Author: Riyadh Alhamad
  */
+
+void execute_command(char *cmd);
 
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	char *cmd;
 
-    while (1)
-    {
-        /* Display prompt */
-        write(STDOUT_FILENO, "$ ", 2);
+	while (1)
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+		nread = getline(&line, &len, stdin);
 
-        /* Read user input */
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
-            free(line);
-            break;
-        }
+		if (nread == -1)
+		{
+			free(line);
+			break;
+		}
 
-        /* Remove newline character */
-        if (line[nread - 1] == '\n')
-            line[nread - 1] = '\0';
+		if (line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
 
-        /* Check if the input is an empty string */
-        if (strlen(line) == 0)
-            continue;
+		if (strlen(line) == 0)
+			continue;
 
-        /* Tokenize input (if there are multiple commands) */
-        char *cmd = strtok(line, " ");
-        if (cmd == NULL)
-            continue;
+		cmd = strtok(line, " ");
+		if (cmd == NULL)
+			continue;
 
-        /* Execute the command */
-        execute_command(cmd);
-    }
+		execute_command(cmd);
+	}
 
-    /* Free the memory allocated for line */
-    free(line);
-    return (0);
+	free(line);
+	return (0);
 }
-
-/**
- * execute_command - Executes the command entered by the user
- * @cmd: The command to be executed
- */
 
 void execute_command(char *cmd)
 {
-    pid_t pid;
-    int status;
+	pid_t pid;
+	int status;
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        exit(1);
-    }
-    else if (pid == 0)
-    {
-        /* Execute the command */
-        if (execve(cmd, &cmd, NULL) == -1)
-        {
-            perror("execve");
-            exit(1);
-        }
-    }
-    else
-    {
-        /* Parent process: wait for child process to finish */
-        wait(&status);
-    }
-}
-
-/**
- * _strdup - Duplicates a string (basic version)
- * @str: The string to duplicate
- * Return: A pointer to the newly allocated string
- */
-
-char *_strdup(const char *str)
-{
-    char *dup = malloc(strlen(str) + 1);
-    if (!dup)
-    {
-        perror("malloc");
-        exit(1);
-    }
-    strcpy(dup, str);
-    return (dup);
-}
-
-/**
- * _strlen - Returns the length of a string
- * @s: The string to measure
- * Return: The length of the string
- */
-
-size_t _strlen(const char *s)
-{
-    size_t length = 0;
-
-    while (*s++)
-        length++;
-
-    return (length);
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		if (execve(cmd, &cmd, NULL) == -1)
+		{
+			perror("execve");
+			exit(1);
+		}
+	}
+	else
+	{
+		wait(&status);
+	}
 }
