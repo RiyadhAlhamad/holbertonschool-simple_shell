b657234f082e176411e3f56563bccdf64bd9c4c9
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-21 15:20:28 +0300

    edit file execute.c

--- execute.c
+++ execute.c
@@ -8,39 +8,45 @@ extern char **environ;
  */
 void execute_command(char *cmd, char *program_name)
 {
-	pid_t pid;
-	char *argv[64];
-	int i = 0;
-	char *token;
+    pid_t pid;
+    char *argv[64];
+    int i = 0;
+    char *token;
 
-	cmd[strcspn(cmd, "\n")] = '\0';
-	token = strtok(cmd, " ");
-	while (token != NULL && i < 63)
-	{
-		argv[i++] = token;
-		token = strtok(NULL, " ");
-	}
-	argv[i] = NULL;
+    /* Remove trailing newline */
+    cmd[strcspn(cmd, "\n")] = '\0';
 
-	pid = fork();
-	if (pid < 0)
-	{
-		perror("fork");
-		return;
-	}
-	if (pid == 0)
-	{
-		if (execve(argv[0], argv, environ) == -1)
-		{
-			write(STDERR_FILENO, program_name, strlen(program_name));
-			write(STDERR_FILENO, ": 1: ", 5);
-			write(STDERR_FILENO, argv[0], strlen(argv[0]));
-			write(STDERR_FILENO, ": not found\n", 13);
-			exit(127);
-		}
-	}
-	else
-	{
-		wait(NULL);
-	}
+    /* Tokenize by spaces */
+    token = strtok(cmd, " ");
+    while (token && i < 63)
+    {
+        argv[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    argv[i] = NULL;
+
+    if (argv[0] == NULL)
+        return;
+
+    pid = fork();
+    if (pid < 0)
+    {
+        perror("fork");
+        return;
+    }
+    if (pid == 0)
+    {
+        if (execve(argv[0], argv, environ) == -1)
+        {
+            write(STDERR_FILENO, program_name, strlen(program_name));
+            write(STDERR_FILENO, ": 1: ", 5);
+            write(STDERR_FILENO, argv[0], strlen(argv[0]));
+            write(STDERR_FILENO, ": not found\n", 13);
+            exit(127);
+        }
+    }
+    else
+    {
+        wait(NULL);
+    }
 }
