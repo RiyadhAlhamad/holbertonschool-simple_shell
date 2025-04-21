2ae792f53542619044bbc777ec41b1b9a9e469a4
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-21 14:57:48 +0300

    Update get_line.c

--- get_line.c
+++ get_line.c
@@ -1,40 +1,33 @@
 #include "shell.h"
 
 /**
- * custom_getline - Custom implementation of getline function
- * @lineptr: Pointer to the line buffer
- * @n: Pointer to the size of the buffer
- * @stream: Input stream (not used, reads from STDIN)
+ * custom_getline - Read up to 1024 chars from STDIN_FILENO
+ * @lineptr: Address of pointer to buffer
+ * @n: Address of size of buffer
+ * @stream: Unused, reading always from STDIN
  *
- * Return: Number of characters read, or -1 on failure
+ * Return: Number of chars read, or -1 on EOF/error
  */
-
 ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream)
 {
-	ssize_t nchars;
+	ssize_t bytes_read;
 	char *buffer;
 
-	(void)stream; /* stream is unused */
-
-	if (lineptr == NULL || n == NULL)
-		return (-1);
+	(void)stream;
 
 	buffer = malloc(1024);
 	if (buffer == NULL)
 		return (-1);
 
-	nchars = read(STDIN_FILENO, buffer, 1024);
-	if (nchars > 0)
-	{
-		buffer[nchars] = '\0';
-		*lineptr = buffer;
-		*n = 1024;
-	}
-	else
+	bytes_read = read(STDIN_FILENO, buffer, 1024);
+	if (bytes_read <= 0)
 	{
 		free(buffer);
 		return (-1);
 	}
 
-	return (nchars);
+	buffer[bytes_read] = '\0';
+	*lineptr = buffer;
+	*n = bytes_read + 1;
+	return (bytes_read);
 }
