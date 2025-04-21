4bf72ec919d9b75ba3aff4943a71a73e0f104bd8
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-21 14:31:07 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,25 +1,14 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-/*
- * File: shell.h
- * Description: Header file for the simple shell project.
- * Contains function prototypes and included libraries.
- */
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
 
-#define MAX_INPUT_SIZE 1024
-
-/* Function Prototypes */
 extern char **environ;
-int _shell(void);
-char *_strdup(const char *str);
 
-#endif /* SHELL_H */
+void execute_command(char *cmd);
+
+#endif
