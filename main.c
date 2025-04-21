a56ecd6cfa79bd427746469f107e31dfa72ef2fc
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-21 14:40:38 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,37 +1,32 @@
 #include "shell.h"
 
 /**
- * main - Entry point of the shell
- * Description: Displays prompt in interactive mode, executes commands
- * @ac: Argument count
- * @av: Argument vector
- * Return: 0 on success
+ * main - Entry point of the simple shell program
+ *
+ * Return: Always 0
  */
+
 int main(int ac, char **av)
 {
 	char *line = NULL;
 	size_t len = 0;
-	ssize_t nread;
+	ssize_t read;
+
 	(void)ac;
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "($) ", 4);
-
-		nread = getline(&line, &len, stdin);
-		if (nread == -1)
+		write(STDOUT_FILENO, "($) ", 4);
+		read = custom_getline(&line, &len, stdin);
+		if (read == -1)
 		{
 			free(line);
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
+			write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
-
-		/* تنفيذ الأمر */
-		execute_command(line, av[0]);
+		if (line[0] != '\n')
+			execute_command(line, av[0]);
 	}
-
 	free(line);
 	return (0);
 }
