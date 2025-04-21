439051ee98829389baf53a36c5f00373dc012916
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-21 14:58:03 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -2,33 +2,39 @@
 extern char **environ;
 
 /**
- * execute_command - Forks and executes the given command string
- * @cmd: The full command string input by user
- * @program_name: Name of the executable program (used in error message)
+ * execute_command - Forks and execve's the given cmd
+ * @cmd: Command line string with args separated by spaces
+ * @program_name: argv[0] of shell, used for error prefix
  */
-
 void execute_command(char *cmd, char *program_name)
 {
 	pid_t pid;
-	char *argv[2];
+	char *argv[64];
+	int i = 0;
+	char *token;
 
 	cmd[strcspn(cmd, "\n")] = '\0';
+	token = strtok(cmd, " ");
+	while (token != NULL && i < 63)
+	{
+		argv[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	argv[i] = NULL;
 
 	pid = fork();
-	if (pid == -1)
+	if (pid < 0)
 	{
-		perror("Fork failed");
+		perror("fork");
 		return;
 	}
 	if (pid == 0)
 	{
-		argv[0] = cmd;
-		argv[1] = NULL;
-		if (execve(cmd, argv, environ) == -1)
+		if (execve(argv[0], argv, environ) == -1)
 		{
 			write(STDERR_FILENO, program_name, strlen(program_name));
 			write(STDERR_FILENO, ": 1: ", 5);
-			write(STDERR_FILENO, cmd, strlen(cmd));
+			write(STDERR_FILENO, argv[0], strlen(argv[0]));
 			write(STDERR_FILENO, ": not found\n", 13);
 			exit(127);
 		}
