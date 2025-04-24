85bfd7db5554236acff49e8e7ee43552821b7572
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-24 19:38:10 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -18,7 +18,7 @@ char **parse_input(char *line);
 char *find_in_path(char *command);
 int execute_command(char **args, char *program_name);
 int is_builtin(char *command);
-int handle_builtin(char **args);
+int handle_builtin(char **args, int *status);
 void print_env(void);
 
 #endif /* SHELL_H */
