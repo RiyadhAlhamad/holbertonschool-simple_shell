55a5826137626cc52d66916dc2d662303f43d0e3
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-24 18:13:52 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,27 +1,33 @@
 #include "shell.h"
 
-void execute_command(char **args) {
+void execute_command(char **args)
+{
     char *full_path = NULL;
 
-    if (strchr(args[0], '/') {
+    if (strchr(args[0], '/') != NULL)
+    {
         full_path = args[0];
     } else {
         full_path = find_in_path(args[0]);
     }
 
-    if (!full_path) {
+    if (!full_path)
+    {
         fprintf(stderr, "%s: command not found\n", args[0]);
         return;
     }
 
     pid_t pid = fork();
-    if (pid == 0) {
+    if (pid == 0)
+    {
         execve(full_path, args, environ);
         perror("execve");
         _exit(EXIT_FAILURE);
-    } else if (pid > 0) {
+    } else if (pid > 0)
+    {
         wait(NULL);
-    } else {
+    } else
+    {
         perror("fork");
     }
 }
