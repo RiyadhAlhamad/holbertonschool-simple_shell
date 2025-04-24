7f6c62a3aef25acadb1a55413f73d07297aaf56c
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-24 20:18:51 +0300

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * is_builtin - Checks if command is a built-in
- * @command: Command to check
+ * is_builtin - Checks if a command is a built-in
+ * @command: The command to check
  * Return: 1 if built-in, 0 otherwise
  */
 int is_builtin(char *command)
@@ -11,28 +11,35 @@ int is_builtin(char *command)
 }
 
 /**
- * handle_builtin - Handles built-in commands
- * @args: Command arguments
- * @last_status: Last command's exit status
+ * handle_builtin - Executes built-in commands
+ * @args: Array of command arguments
+ * @last_status: Exit status of last command
+ * Return: 1 if exit command was handled, 0 otherwise
  */
-void handle_builtin(char **args, int last_status)
+int handle_builtin(char **args, int last_status)
 {
-    if (strcmp(args[0], "exit") == 0)
-    {
-        exit(last_status);
-    }
-    else if (strcmp(args[0], "env") == 0)
-    {
-        print_env();
-    }
+	if (strcmp(args[0], "exit") == 0)
+	{
+		/* Free resources before exiting */
+		free(args);
+		exit(last_status);
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		print_env();
+	}
+	return (0);
 }
 
 /**
- * print_env - Prints environment variables
+ * print_env - Prints all environment variables
  */
 void print_env(void)
 {
 	char **env = environ;
+
+	if (!env)
+		return;
 
 	while (*env)
 	{
