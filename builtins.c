4afae8a01439a1b29e710ee684270445d486d3cf
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-24 19:32:16 +0300

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -7,24 +7,37 @@
  */
 int is_builtin(char *command)
 {
-    return (strcmp(command, "exit") == 0 || strcmp(command, "env") == 0);
+	return (strcmp(command, "exit") == 0 || strcmp(command, "env") == 0);
 }
 
 /**
  * handle_builtin - Handles built-in commands
  * @args: Array of command arguments
- * Return: 1 if exit command, 0 otherwise
+ * Return: 1 for exit command, 0 otherwise
  */
 int handle_builtin(char **args)
 {
-    if (strcmp(args[0], "exit") == 0)
-    {
-        return (1);
-    }
-    else if (strcmp(args[0], "env") == 0)
-    {
-        print_env();
-    }
-    return (0);
+	if (strcmp(args[0], "exit") == 0)
+	{
+		return (1);
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		print_env();
+	}
+	return (0);
 }
+
+/**
+ * print_env - Prints environment variables
+ */
+void print_env(void)
+{
+	char **env = environ;
+
+	while (*env)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
 }
