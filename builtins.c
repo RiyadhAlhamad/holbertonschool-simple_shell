37039636a842d7d0d4e57067586d1602ac75dd99
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-24 20:27:21 +0300

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * is_builtin - Checks if a command is a built-in
- * @command: The command to check
+ * is_builtin - Checks if command is built-in
+ * @command: Command to check
  * Return: 1 if built-in, 0 otherwise
  */
 int is_builtin(char *command)
@@ -11,35 +11,26 @@ int is_builtin(char *command)
 }
 
 /**
- * handle_builtin - Executes built-in commands
- * @args: Array of command arguments
- * @last_status: Exit status of last command
- * Return: 1 if exit command was handled, 0 otherwise
+ * handle_builtin - Handles built-in commands
+ * @args: Command arguments
+ * @last_status: Last command's exit status
+ * Return: 1 if exit command, 0 otherwise
  */
 int handle_builtin(char **args, int last_status)
 {
 	if (strcmp(args[0], "exit") == 0)
-	{
-		/* Free resources before exiting */
-		free(args);
-		exit(last_status);
-	}
+		return (1);
 	else if (strcmp(args[0], "env") == 0)
-	{
 		print_env();
-	}
 	return (0);
 }
 
 /**
- * print_env - Prints all environment variables
+ * print_env - Prints environment variables
  */
 void print_env(void)
 {
 	char **env = environ;
-
-	if (!env)
-		return;
 
 	while (*env)
 	{
