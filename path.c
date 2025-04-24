37e8ccf58b88c2267be1d55abbd67f772bcaace6
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-24 18:24:47 +0300

    Update path.c

--- path.c
+++ path.c
@@ -1,21 +1,44 @@
 #include "shell.h"
 
-char *find_in_path(char *command) {
-    char *path = getenv("PATH");
-    char *path_copy = strdup(path);
-    char *dir = strtok(path_copy, ":");
-    char *full_path = malloc(BUFFER_SIZE);
+/**
+ * find_in_path - Searches for command in PATH environment variable
+ * @command: Command to search for
+ * 
+ * Return: Full path if found, NULL otherwise
+ */
+char *find_in_path(char *command)
+{
+    char *path, *path_copy, *dir, *full_path;
+    int cmd_len, dir_len;
 
-    while (dir != NULL) {
-        snprintf(full_path, BUFFER_SIZE, "%s/%s", dir, command);
-        if (access(full_path, X_OK) == 0) {
-            free(path_copy);
-            return full_path;
-        }
+    path = getenv("PATH");
+    if (!path)
+        return (NULL);
+
+    path_copy = strdup(path);
+    if (!path_copy)
+        return (NULL);
+
+    cmd_len = strlen(command);
+    dir = strtok(path_copy, ":");
+    full_path = NULL;
+
+    while (dir)
+    {
+        dir_len = strlen(dir);
+        full_path = malloc(dir_len + cmd_len + 2);
+        if (!full_path)
+            break;
+
+        snprintf(full_path, dir_len + cmd_len + 2, "%s/%s", dir, command);
+        if (access(full_path, X_OK) == 0)
+            break;
+
+        free(full_path);
+        full_path = NULL;
         dir = strtok(NULL, ":");
     }
 
-    free(full_path);
     free(path_copy);
-    return NULL;
+    return (full_path);
 }
