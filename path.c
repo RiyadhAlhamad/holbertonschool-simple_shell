0923accb64311c93bb3ebdb291c2fd2392fc1e35
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-24 20:45:29 +0300

    edit some logic code path.c

--- path.c
+++ path.c
@@ -24,14 +24,22 @@ char *find_in_path(char *command)
 		return (NULL);
 
 	path_copy = strdup(path);
-	dir = strtok(path_copy, ":");
+	if (!path_copy)
+		return (NULL);
 
+	dir = strtok(path_copy, ":");
 	while (dir)
 	{
 		int dir_len = strlen(dir);
 		int cmd_len = strlen(command);
 
 		full_path = malloc(dir_len + cmd_len + 2);
+		if (!full_path)
+		{
+			free(path_copy);
+			return (NULL);
+		}
+
 		snprintf(full_path, dir_len + cmd_len + 2, "%s/%s", dir, command);
 
 		if (access(full_path, X_OK) == 0)
