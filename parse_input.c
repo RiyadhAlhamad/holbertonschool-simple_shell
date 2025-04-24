bae75919afda17233dfe5e0f10876ee1c385125a
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-24 18:24:06 +0300

    Update parse_input.c

--- parse_input.c
+++ parse_input.c
@@ -1,15 +1,30 @@
 #include "shell.h"
 
-char **parse_input(char *line) {
+/**
+ * parse_input - Splits input string into command arguments
+ * @line: Input string to parse
+ * 
+ * Return: NULL-terminated array of arguments
+ */
+char **parse_input(char *line)
+{
     char **args = malloc(MAX_ARGS * sizeof(char *));
+    char *token;
     int i = 0;
-    char *token = strtok(line, " ");
 
-    while (token != NULL && i < MAX_ARGS - 1) {
+    if (!args)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, " ");
+    while (token != NULL && i < MAX_ARGS - 1)
+    {
         args[i++] = token;
         token = strtok(NULL, " ");
     }
     args[i] = NULL;
 
-    return args;
+    return (args);
 }
