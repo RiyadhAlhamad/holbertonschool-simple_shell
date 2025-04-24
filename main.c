ed139a8f4a915c1c81edfa3cfb8e42b4cd7167e1
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-24 20:27:08 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,52 +1,58 @@
 #include "shell.h"
 
 /**
- * main - Entry point of the shell
+ * main - Entry point of simple shell
  * @argc: Argument count
  * @argv: Argument vector
  * Return: Last command's exit status
  */
 int main(int argc, char **argv)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    char **args = NULL;
-    int status = 0;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char **args = NULL;
+	int status = 0;
 
-    (void)argc;
+	(void)argc;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            printf("$ ");
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
 
-        read = getline(&line, &len, stdin);
-        if (read == -1)
-            break;
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+			break;
 
-        line[strcspn(line, "\n")] = '\0';
-        args = parse_input(line);
+		line[strcspn(line, "\n")] = '\0';
+		args = parse_input(line);
 
-        if (args[0])
-        {
-            if (is_builtin(args[0]))
-            {
-                int should_exit = handle_builtin(args, status);
-                free(args);
-                free(line);
-                if (should_exit)
-                    exit(status);
-            }
-            else
-            {
-                status = execute_command(args, argv[0]);
-            }
-        }
-        free(args);
-        args = NULL;
-    }
+		if (args[0])
+		{
+			if (is_builtin(args[0]))
+			{
+				if (handle_builtin(args, status))
+				{
+					free(args);
+					free(line);
+					exit(status);
+				}
+				free(args);
+			}
+			else
+			{
+				status = execute_command(args, argv[0]);
+				free(args);
+			}
+		}
+		else
+		{
+			free(args);
+		}
+		args = NULL;
+	}
 
-    free(line);
-    return (status);
+	free(line);
+	return (status);
 }
