7e714c32ec13d966b23a09fcaa8f6bd9ef078cc8
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-24 19:32:31 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -4,7 +4,7 @@
  * execute_command - Executes a command with arguments
  * @args: Array of command arguments
  * @program_name: Name of the shell program
- * Return: Exit status
+ * Return: Exit status code
  */
 int execute_command(char **args, char *program_name)
 {
@@ -12,26 +12,35 @@ int execute_command(char **args, char *program_name)
 	pid_t pid;
 	int status = 0, exit_status = 0;
 
-	if (strchr(args[0], '/') != NULL) {
+	if (strchr(args[0], '/') != NULL)
+	{
 		full_path = args[0];
-	} else {
+	}
+	else
+	{
 		full_path = find_in_path(args[0]);
 	}
 
-	if (!full_path) {
+	if (!full_path)
+	{
 		fprintf(stderr, "%s: 1: %s: not found\n", program_name, args[0]);
 		return (127);
 	}
 
 	pid = fork();
-	if (pid == -1) {
+	if (pid == -1)
+	{
 		perror("fork");
 		exit(EXIT_FAILURE);
-	} else if (pid == 0) {
+	}
+	else if (pid == 0)
+	{
 		execve(full_path, args, environ);
 		perror("execve");
 		_exit(EXIT_FAILURE);
-	} else {
+	}
+	else
+	{
 		waitpid(pid, &status, 0);
 		if (WIFEXITED(status))
 			exit_status = WEXITSTATUS(status);
