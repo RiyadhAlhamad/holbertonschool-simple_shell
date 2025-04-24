72a28d28b157909ed8ef8aabfee241db31042def
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-24 18:01:41 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,129 +1,27 @@
 #include "shell.h"
 
-/**
- * find_command - Searches for a command in the directories listed in PATH
- * @cmd: Command name to search for
- *
- * Return: Full path to the command if found, NULL otherwise
- */
-char *find_command(char *cmd)
-{
-	char *path = NULL;
-	char *token;
-	char full_path[1024];
-	int i = 0;
+void execute_command(char **args) {
+    char *full_path = NULL;
 
-	while (environ[i])
-	{
-		if (strncmp(environ[i], "PATH=", 5) == 0)
-		{
-			path = strdup(environ[i] + 5);
-			break;
-		}
-		i++;
-	}
+    if (strchr(args[0], '/') {
+        full_path = args[0];
+    } else {
+        full_path = find_in_path(args[0]);
+    }
 
-	if (!path)
-		return (NULL);
+    if (!full_path) {
+        fprintf(stderr, "%s: command not found\n", args[0]);
+        return;
+    }
 
-	token = strtok(path, ":");
-	while (token)
-	{
-		snprintf(full_path, sizeof(full_path), "%s/%s", token, cmd);
-		if (access(full_path, X_OK) == 0)
-		{
-			free(path);
-			return (strdup(full_path));
-		}
-		token = strtok(NULL, ":");
-	}
-
-	free(path);
-	return (NULL);
-}
-
-/**
- * execute_command - Forks and executes a given command string with arguments
- * @cmd: Command line string (may include args separated by spaces or tabs)
- * @program_name: Shell program name (argv[0]) for error messages
- *
- * Return: Exit status of executed command
- */
-int execute_command(char *cmd, char *program_name)
-{
-	pid_t pid;
-	char *argv[MAX_ARGS];
-	int i = 0, wstatus;
-	char *token;
-	char *command_path = NULL;
-	int exit_status = 0;
-
-	/* strip newline */
-	cmd[strcspn(cmd, "\n")] = '\0';
-
-	/* split into argv */
-	token = strtok(cmd, " \t");
-	while (token && i < MAX_ARGS - 1)
-	{
-		argv[i++] = token;
-		token = strtok(NULL, " \t");
-	}
-	argv[i] = NULL;
-
-	if (!argv[0])
-		return (0);
-
-	if (strcmp(argv[0], "exit") == 0)
-	  {
-	    if (argv[1] != NULL)
-	      {
-		write(STDERR_FILENO, "exit: too many arguments\n", 25);
-		return (1);
-	      }
-	  exit(0);
-	  }
-	/* if name contains '/', treat as path; else search PATH */
-	if (strchr(argv[0], '/'))
-	{
-		if (access(argv[0], X_OK) == 0)
-			command_path = strdup(argv[0]);
-		else
-			command_path = NULL;
-	}
-	else
-		command_path = find_command(argv[0]);
-
-	pid = fork();
-	if (pid < 0)
-	{
-		perror("fork");
-		free(command_path);
-		return (127);
-	}
-	else if (pid == 0)
-	{
-		/* Child */
-		if (!command_path)
-		{
-			write(STDERR_FILENO, program_name, strlen(program_name));
-			write(STDERR_FILENO, ": 1: ", 5);
-			write(STDERR_FILENO, argv[0], strlen(argv[0]));
-			write(STDERR_FILENO, ": not found\n", 12);
-			exit(127);
-		}
-		execve(command_path, argv, environ);
-		perror("execve");
-		free(command_path);
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		/* Parent waits and records exit status */
-		wait(&wstatus);
-		if (WIFEXITED(wstatus))
-			exit_status = WEXITSTATUS(wstatus);
-	}
-
-	free(command_path);
-	return (exit_status);
+    pid_t pid = fork();
+    if (pid == 0) {
+        execve(full_path, args, environ);
+        perror("execve");
+        _exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        wait(NULL);
+    } else {
+        perror("fork");
+    }
 }
