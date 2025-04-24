1d6a355c1dd6d36d6a1a1f70f80bf62453921e14
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-24 19:02:00 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -7,41 +7,32 @@
  */
 void execute_command(char **args, char *program_name)
 {
-    char *full_path = NULL;
-    pid_t pid;
+	char *full_path = NULL;
+	pid_t pid;
 
-    if (strchr(args[0], '/') != NULL)
-    {
-        full_path = args[0];
-    }
-    else
-    {
-        full_path = find_in_path(args[0]);
-    }
+	if (strchr(args[0], '/') != NULL) {
+		full_path = args[0];
+	} else {
+		full_path = find_in_path(args[0]);
+	}
 
-    if (!full_path)
-    {
-        fprintf(stderr, "%s: 1: %s: not found\n", program_name, args[0]);
-        return;
-    }
+	if (!full_path) {
+		fprintf(stderr, "%s: 1: %s: not found\n", program_name, args[0]);
+		return;
+	}
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        execve(full_path, args, environ);
-        perror("execve");
-        _exit(EXIT_FAILURE);
-    }
-    else
-    {
-        wait(NULL);
-    }
+	pid = fork();
+	if (pid == -1) {
+		perror("fork");
+		exit(EXIT_FAILURE);
+	} else if (pid == 0) {
+		execve(full_path, args, environ);
+		perror("execve");
+		_exit(EXIT_FAILURE);
+	} else {
+		wait(NULL);
+	}
 
-    if (full_path != args[0])
-        free(full_path);
+	if (full_path != args[0])
+		free(full_path);
 }
